869fa11bfe12787f996b95de38842dcf07f63f49
ahyahya1616 <ahmaneyahya10@gmail.com>
2026-01-11 00:34:53 +0100

    modify app.config.ts

--- apps/public-app/src/app/app.config.ts
+++ apps/public-app/src/app/app.config.ts
@@ -8,13 +8,8 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { authInterceptor } from './features/auth/interceptors/auth.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
-<<<<<<< HEAD
 import { provideAnimations } from '@angular/platform-browser/animations';
-=======
 
-
-/**/
->>>>>>> 568124097e22afc0d72f8eb11dcf13495e9c362e
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
